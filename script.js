3d9449d9e43731a49e234fa4a3312beb635b6301
Rahulroy-kolkata <roy79788@gmail.com>
2026-07-09 15:41:49 +0530

    Remove password strength checking function
    Removed the checkPasswordStrength function from script.js.

--- script.js
+++ script.js
@@ -314,33 +314,3 @@ document.getElementById("randomResult").innerHTML =
 "🎉 Random Number: " + random;
 
 }
-function checkPasswordStrength(){
-
-let password = document.getElementById("passwordCheck").value;
-
-let result = document.getElementById("strengthResult");
-
-if(password.length === 0){
-result.innerHTML = "Please Enter Password";
-return;
-}
-
-let strength = 0;
-
-if(password.length >= 8) strength++;
-if(/[A-Z]/.test(password)) strength++;
-if(/[a-z]/.test(password)) strength++;
-if(/[0-9]/.test(password)) strength++;
-if(/[^A-Za-z0-9]/.test(password)) strength++;
-
-if(strength <= 2){
-result.innerHTML = "🔴 Weak Password";
-}
-else if(strength <= 4){
-result.innerHTML = "🟡 Medium Password";
-}
-else{
-result.innerHTML = "🟢 Strong Password";
-}
-
-}
